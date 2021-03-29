b47ea45fc05778f51cb8781018bc7e7e5f7c7274
Zulfadhli <zfaba.a@gmail.com>
2021-03-29 15:57:18 +0800

    Group Layer Control

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { LayersControl, MapContainer, TileLayer, WMSTileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 function App() {
   return (
-    <MapContainer center={[50, 50]} zoom={4}>
+    <MapContainer center={[45, -90]} zoom={8}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    </MapContainer>
+      <LayersControl position="topright">
+        <LayersControl.Overlay name="Polyline Landmark">
+          <WMSTileLayer url="http://localhost:8080/geoserver/tiger/wms?" params={{
+            layers: 'tiger:poly_landmarks',
+            transparent: true,
+            format: 'image/png'
+          }} />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Road">
+          <WMSTileLayer url="http://localhost:8080/geoserver/tiger/wms?" params={{
+            layers: 'tiger:tiger_roads',
+            transparent: true,
+            format: 'image/png'
+          }} />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Place of Interest">
+          <WMSTileLayer url="http://localhost:8080/geoserver/tiger/wms?" params={{
+            layers: 'tiger:poi',
+            transparent: true,
+            format: 'image/png'
+          }} />
+        </LayersControl.Overlay>
+      </LayersControl>
+    </MapContainer >
   );
 }
 
