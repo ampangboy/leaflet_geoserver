b9e1a84fd5162add3dd9b164b60ab9ee3c8bd454
Zulfadhli <zfaba.a@gmail.com>
2021-03-29 10:19:01 +0800

    Adding WMS layer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, WMSTileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 function App() {
   return (
     <MapContainer center={[50, 50]} zoom={4}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <WMSTileLayer url="http://localhost:8080/geoserver/nurc/wms" params={
+        {
+          layers: "nurc:Img_Sample",
+          transparent: true,
+          format: 'image/png',
+        }
+      } />
+
+
     </MapContainer>
   );
 }
