8b0dee64d1f8a2fc22f093a83ad1cc9313f3cd6c
Zulfadhli <zfaba.a@gmail.com>
2021-03-30 15:09:38 +0800

    Add use ref to square component

--- src/customComponent.js
+++ src/customComponent.js
@@ -1,20 +1,28 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { useLeafletContext } from '@react-leaflet/core'
 import * as L from 'leaflet'
 
+
+function getBounds(props) {
+    return L.latLng(props.center).toBounds(props.size)
+}
+
 function Square(props) {
     const context = useLeafletContext()
+    // use ref here causing the square element DOM is taken out from the Virtual DOM. Hence,
+    // any manipulation to the react component is not re-render here. 
+    const squareRef = useRef()
 
     useEffect(() => {
-        const bounds = L.latLng(props.center).toBounds(props.size)
-        const square = new L.Rectangle(bounds)
+        squareRef.current = new L.Rectangle(getBounds(props))
+        console.log(squareRef)
         const container = context.layerContainer || context.map
-        container.addLayer(square)
+        container.addLayer(squareRef.current)
 
         return () => {
-            container.removeLayer(square)
+            container.removeLayer(squareRef.current)
         }
-    })
+    }, [])
 
     return null
 }
